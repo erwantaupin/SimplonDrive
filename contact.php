22f65e93c3341bcabf2762c13d3390445a721fca
K-vin <vinz.sosaity@gmail.com>
2022-02-01 16:16:58 +0100

    commit

---
+++ contact.php
@@ -0,0 +1,33 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Document</title>
+</head>
+<body>
+<form action="/my-handling-form-page" method="post">
+
+</form>
+<form action="/ma-page-de-traitement" method="post">
+    <div>
+        <label for="name">Nom :</label>
+        <input type="text" id="name" name="user_name">
+    </div>
+    <div>
+        <label for="mail">e-mail :</label>
+        <input type="email" id="mail" name="user_mail">
+    </div>
+    <div>
+        <label for="msg">Message :</label>
+        <textarea id="msg" name="user_message"></textarea>
+    </div>
+</form>
+<textarea>par défaut cet élément sera renseigné avec ce texte</textarea>
+<div class="button">
+        <button type="submit">Envoyer le message</button>
+    </div>
+
+</body>
+</html>
