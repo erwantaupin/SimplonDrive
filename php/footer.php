f01210ea18b5140110b1f51f72cc9df4d281a2ff
mikaelhen <mikaelhen28@gmail.com>
2022-02-03 16:34:21 +0100

    footer 16h33

---
+++ php/footer.php
@@ -0,0 +1,36 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <title>Document</title>
+
+</head>
+<body>
+    <div class="footer">
+        <div class="menu">
+            <ul class="ul">        
+                <li><a href="premiermenu" title="page_d'accueil">ACCUEIL</a></li>
+                <li><a href="deuxiememenu" title="page-boutique">BOUTIQUE</a></li>
+                <li><a href="troisiememenu" title="page-contact">CONTACT</a></li>
+            </ul>
+        </div>
+
+        <div class="logo">
+                <img src="../assets/img/logo-removebg-preview.png" class="logo">
+                <a class="logo"></a>
+        </div>
+       
+        <div class="h5">
+        <H5>Comande et Livraison Drive</H5>
+        </div>
+
+        <div class="copyright">
+        <p>© 2022 site réalisé par erwan-kevin-mikael</p>
+        </div>
+    </div>  
+    
+    
+</body>
+</html>
