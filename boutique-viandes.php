640f6e11d3a075b70a28a0c5bd8fdcb5aaae39cf
erwantaupin <taupin.erwan@laposte.net>
2022-02-04 20:20:17 +0100

    commit menu boutique

--- boutique-viandes.php
+++ boutique-viandes.php
@@ -4,14 +4,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/style.css">
-    <title>boutique</title>
+    <title>boutique/viande</title>
 </head>
 <body>
         <?php include "assets/php/header.php" ?>
-    <div class="navcontainer">
+            <div class="buttonreturn">
+                <div class="return"><li><a href="boutique.php" title="return"> <img src="assets/img/retour.png"></a></li></div>
+            </div>
+            <div class="navcontainer boutique">
                 <div class="viande"><li><a href="boutique-viandes.php" title="VIANDE"> <img src="assets/img/Boeuf.jpeg"><h3>VIANDES</h3></a></li></div>
                 <div class="legume"><li><a href="boutique-legumes.php" title="LEGUME"><img src="assets/img/legume.jpg"><h3>LEGUMES</h3></a></li></div>
                 <div class="desserts"><li><a href="boutique-desserts.php" title="DESSERTS"><img src="assets/img/desserts.jpg"><h3>DESSERT</h3></a></li></div>
-    </div>
+                <div class="legume"><li><a href="boutique-legumes.php" title="LEGUME"><img src="assets/img/legume.jpg"><h3>LEGUMES</h3></a></li></div>
+                <div class="desserts"><li><a href="boutique-desserts.php" title="DESSERTS"><img src="assets/img/desserts.jpg"><h3>DESSERT</h3></a></li></div>
+            </div>
+        <?php include "assets/php/footer.php" ?>
 </body>
 </html>
