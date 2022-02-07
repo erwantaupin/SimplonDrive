2cfb7babbd47e4ac8361451290269fc6f3ad777c
mikaelhen <mikaelhen28@gmail.com>
2022-02-07 15:18:33 +0100

    maj 15h18

---
+++ php/index.php
@@ -0,0 +1,40 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <title>accueil</title>
+</head>
+<body>
+    <div class="boxbienvenue">
+    <div class="bienvenu">
+        <div class="text">
+            Bienvenu sur le site SimplonDrive<br> Faites vos courses simplement et nous pouvons vous livrer.
+        </div>
+    </div>
+    </div>
+    <div class="boxcontent">
+    <div class="contenant">
+    <div class="boxhoraire">
+        <div class="horaire">
+        <h6>Horaire d'ouverture</h6>
+        </div>
+    <div class="borderheure">
+        <div class="heure">
+        Lundi 8h00 à 19h00<br>
+        Mardi 8h00 à 19h00<br>
+        Mercredi 8h00 à 19h00<br>
+        Jeudi 8h00 à 19h00<br>
+        Vendredi 8h00 à 19h00<br>
+        Fermé Samedi et Dimanche
+        </div>
+    </div>
+</div>
+        <div class="logo_drive">
+        <img src="../assets/img/logodrive.png" alt="">
+    </div>
+</div>
+
+</body>
+</html>
